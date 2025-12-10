16ec645fd631d40421f38f32de371e5ff9d9dec2
Matt Parker <matt.parker@hinge.co>
2025-12-10 13:28:06 -0500

    implemented it in binary award

--- 09/main.go
+++ 09/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -28,18 +28,56 @@ func mustParseInt(s string) int {
 
 func main() {
 	points := helpers.MustParseTo(helpers.InputFile(), transform)
-	maxDistance := -1.0
-	for i, self := range points {
-		for j, other := range points {
-			if i == j {
-				continue
-			}
-			distance := (math.Abs(float64(self.x - other.x)) + 1) * (math.Abs(float64(self.y - other.y) + 1))
-			if distance > maxDistance {
-				maxDistance = distance
-			}
+	compressedYCoordinates := make(map[int]int)
+	compressedXCoordinates := make(map[int]int)
+	sort.Slice(points, func(i, j int) bool {
+		return points[i].y < points[j].y
+	})
+	xPoints := make([]Point, len(points))
+	copy(xPoints, points)
+	// reduce xPoints, remove dupes
+
+	sort.Slice(xPoints, func(i, j int) bool {
+		return xPoints[i].x < xPoints[j].x
+	})
+	currCord := 0
+	for _, point := range xPoints {
+		if _, ok := compressedXCoordinates[point.x]; ok {
+			continue	
 		}
+		compressedXCoordinates[point.x] = currCord
+		currCord++
 	}
-	distanceInt := int64(maxDistance)
-	fmt.Println(distanceInt)
+	yPoints := make([]Point, len(points))
+	copy(yPoints, points)
+	sort.Slice(yPoints, func(i, j int) bool {
+		return yPoints[i].y < yPoints[j].y
+	})
+	currCord = 0 
+	for _, point := range yPoints {
+		if _, ok := compressedYCoordinates[point.y]; ok {
+			continue
+		}
+		compressedYCoordinates[point.y] = currCord
+		currCord++
+	}
+
+	// and now, we can proceed with dumbass brute force
+	board := make([][]string, len(compressedYCoordinates))
+	for i := range board {
+		board[i] = make([]string, len(compressedXCoordinates))
+	}
+	for _, point := range points {
+		board[compressedYCoordinates[point.y]][compressedXCoordinates[point.x]] = "#"
+	}
+	for _, row := range board {
+		fmt.Println(row)
+	}
+
+
+
+
+
+
+
 }
